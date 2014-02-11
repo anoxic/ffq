2cb75558ab0c5a7f70747b1cd149929aa4e31cb3
Brian <brian@endor.(none)>
2014-02-11 11:00:52 +0000

    added auth

--- index.php
+++ index.php
@@ -5,18 +5,70 @@ function name($_) {
 	return "pages/".preg_replace("/[^a-zA-Z0-9]/", "~", $_).".md";
 }
 
+function g($prop) {
+	if (isset($_POST[$prop])) return $_POST[$prop];
+	if (isset($_GET[$prop])) return $_GET[$prop];
+}
+
+function s($prop, $val = false) {
+	session_start();
+
+	if ($val != false)           $_SESSION[$prop] = $val;
+	if (isset($_SESSION[$prop])) return $_SESSION[$prop];
+
+	session_write_close();
+}
+
 function render($data, $template) {
 
 }
+
+function auth() {
+	if (s('user') == null) {
+		redirect(substr_replace(request_path(), '=', 1,0));
+	}
+}
+
+before(function() {
+	$type = substr(request_path(), 1,1);
+
+	if ($type == '%' || $type == '@') {
+		auth();
+	}
+});
+
+get('/-', function() { session_start(); $_SESSION = []; session_destroy(); });
 
 get('/~<*:page>', function($_) {
 	$file = name($_);
 
 	if (file_exists($file)) {
-		echo nl2br(file_get_contents($file));
+		header("Content-type: text/plain");
+		echo file_get_contents($file);
 	} else {
 		halt(404);
 	}
+});
+
+form('/=<*:page>', function($_) {
+	$users = ["brian test", "jim pass"];
+
+	if (request_method('POST')) {
+		foreach ($users as $u) {
+			if ($u == g('user')." ".g('pass')) {
+				s('user', g('user'));
+				redirect($_);
+			}
+		}
+
+		flash('error', 'Username or password does not match :(');
+		flash('user', g('user'));
+		redirect();
+	}
+
+	echo flash('error')."<form method=post>".csrf_field()
+	    ."<input autofocus name=user value=\"".flash('user')."\"><input name=pass type=password>"
+	    ."<button>&gt;</button></form>";
 });
 
 form('/@<*:page>', function($_) {
