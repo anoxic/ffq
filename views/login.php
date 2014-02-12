2d589a8e2b223362c5e0c7a4bfa068611131bdde
Brian <brian@endor.(none)>
2014-02-12 04:07:22 +0000

    updated login screen

--- views/login.php
+++ views/login.php
@@ -1,10 +1,52 @@
-<?= $error ?>
-<?= $alert ?>
-<?= $notice ?>
+<style>
+body {
+	margin: 0;
+}
+input {
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	padding: 0;
+	margin: 0;
+	border: 0;
+	outline: 0;
+	width: 50%;
+	text-align: center;
+}
+.u {
+	left: 0;
+}
+.g {
+	right: 0;
+	border-left: 2px solid #EAEFF1;
+}
+button {
+	height: 2em;
+	line-height: 1.6em;
+	padding: 0 1em;
+	position: absolute;
+	bottom: 0.3em;
+	right: 0.5em;
+}
+.msg {
+	position: absolute;
+	text-align: center;
+	width: 100%;
+	margin: 0;
+	line-height: 3.6em;
+}
+</style>
 
 <form method=post>
 	<?= $csrf_field ?>
-	<input autofocus name=user value="<?=$user?>">
-	<input name=pass type=password>
-	<button>&gt;</button>
+	<input class=u placeholder=Username autofocus name=user value="<?=$user?>">
+	<input class=g placeholder=Password name=pass type=password>
+	<button>Login</button>
 </form>
+
+<p class=msg>
+<?= $error ?>
+<?= $alert ?>
+<?= $notice ?>
+</p>
+
