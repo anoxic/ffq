51e6248bfeb3bd278cac5c37c6d73bb0abe5e173
anoxic <brian@zickzickzick.com>
2014-05-02 05:27:20 +0000

    Simplify login view

--- views/login.php
+++ views/login.php
@@ -1,41 +1,29 @@
 <style>
 body {
-	margin: 0;
-}
-input {
-	position: absolute;
-	top: 0;
-	bottom: 0;
-	padding: 0;
-	margin: 0;
-	border: 0;
-	outline: 0;
-	width: 50%;
-	text-align: center;
-}
-.u {
-	left: 0;
-}
-.g {
-	right: 0;
-	border-left: 2px solid #EAEFF1;
+    margin: 7em;
+    text-align: center;
 }
 button {
 	height: 2em;
 	line-height: 1.6em;
 	padding: 0 1em;
-	position: absolute;
-	bottom: 0.3em;
-	right: 0.5em;
 }
 .msg {
-	position: absolute;
 	text-align: center;
 	width: 100%;
 	margin: 0;
 	line-height: 3.6em;
+    position: absolute;
+    top: 0;
+    left: 0;
 }
 </style>
+
+<p class=msg>
+<?= $error ?>
+<?= $alert ?>
+<?= $notice ?>
+</p>
 
 <form method=post>
 	<?= $csrf_field ?>
@@ -43,10 +31,4 @@ button {
 	<input class=g placeholder=Password name=pass type=password>
 	<button>Login</button>
 </form>
-
-<p class=msg>
-<?= $error ?>
-<?= $alert ?>
-<?= $notice ?>
-</p>
 
