8d7063297c60d901b6439f05d8a74c27a78d6d83
anoxic <brian@zickzickzick.com>
2014-06-23 13:07:55 +0000

    Remove unused code

--- lib/page/filename.php
+++ lib/page/filename.php
@@ -1,8 +1,7 @@
 <?php
 // filename creates the pathname of a wiki page
 function filename($n = "", $prefix = "pages/") {
-    if ($n < 0) $prefix = "";
-    if (empty($n) || $n < 0) $n = substr(request_path(), 1);
+    if (empty($n)) $n = substr(request_path(), 1);
 
     return $prefix . preg_replace("; +;", " ", preg_replace(";/;", ".", 
         preg_replace(";[^a-z.];", "-", strtolower($n))));
