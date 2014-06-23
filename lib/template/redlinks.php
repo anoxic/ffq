9cfc42ddedd2670543ce2a29bb62dec62c59e3d3
anoxic <brian@zickzickzick.com>
2014-06-23 12:56:25 +0000

    Rewrite lib/templates/redlinks.php

--- lib/template/redlinks.php
+++ lib/template/redlinks.php
@@ -2,18 +2,9 @@
 // redlinks checks if internal links in a text are broken,
 // and if so adds the class "redlink" to them
 function redlinks($_) {
-    preg_match_all("/<a href=\"\/[^'\">]+\">/", $_, $links);
-
-    foreach ($links[0] as $link) {
-        preg_match("/(?<=href=\"\/)[^\"]+(?=\")/", $link, $file);
-
-        if (!is_link(filename($file[0])))
-            $reps[$link] = preg_replace("/<a/", "<a class=redlink", $link);
-    }
-
-    if (isset($reps)) foreach ($reps as $a=>$b)
-        $_ = preg_replace("|".$a."|", $b, $_);
-
-    return $_;
+    return preg_replace_callback("/<a href=\"?\/([^'\">]+)\"?>/", function($matches){
+        if (!is_link(filename($matches[1])))
+            return preg_replace("/<a/", "<a class=redlink", $matches[0]);
+    }, $_);
 }
 
