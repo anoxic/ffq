16fe6e228b326529a19b3fa277e0d61b94257cbc
anoxic <brian@zickzickzick.com>
2014-07-03 19:31:08 +0000

    Add viewport metainfo for phones

--- views/view.php
+++ views/view.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<meta charset=utf-8>
+    <meta name=viewport content="width=device-width, initial-scale=1">
 	<title><?=$name?> * wiki.zick.io</title>
     <style><?php require("src/wiki.css"); ?></style>
 </head>
