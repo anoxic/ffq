94d5f2aba7a081db7f133bddb008f41df081123f
anoxic <brian@zickzickzick.com>
2014-07-06 10:47:41 +0000

    Local link are not broken

--- lib/template/redlinks.php
+++ lib/template/redlinks.php
@@ -3,7 +3,9 @@
 // and if so adds the class "redlink" to them
 function redlinks($_) {
     return preg_replace_callback("/<a href=\"?\/([^'\">]+)\"?>/", function($matches){
-        if (!is_link(filename($matches[1])))
+        if (is_link(filename($matches[1])))
+            return $matches[0];
+        else
             return preg_replace("/<a/", "<a class=redlink", $matches[0]);
     }, $_);
 }
