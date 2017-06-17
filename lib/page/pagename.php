010d013040e808f508391312219ebfead42e2328
anoxic <brian@zickzickzick.com>
2017-06-17 14:53:10 -0400

    expand and simplify pagename()

--- lib/page/pagename.php
+++ lib/page/pagename.php
@@ -1,7 +1,8 @@
 <?php
 // pagename creates a "human" page name from a filename
 function pagename($n) {
-    return ucwords($n = preg_replace(";^ ;", "~",
-        str_replace("-", " ", str_replace(".", "/", $n))));
+    $n = preg_replace("/^-/", "~", $n);
+    $n = strtr($n, "-.", " /");
+    return $n;
 }
 
