5131a506923c3867494855162b665c8e1f440317
Brian Zick <brian@zick.io>
2019-04-16 21:23:08 -0400

    use house icon to go home (v up arrow)

--- views/partials/toolbar.php
+++ views/partials/toolbar.php
@@ -1,6 +1,6 @@
 <div class=toolbar>
 <div class=wrapper>
-<a href=/ title=root>⇧</a>
+<a href=/ title=root>⌂</a>
 <a href=<?=partial("new_page.php")?> title=new>+</a>
 <a href=/- class=logout title=logout>⎋</a>
 </div>
