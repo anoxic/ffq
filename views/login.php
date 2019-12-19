f36c9d41f9011138b7cd3269cda59bb5bc1fcc44
Brian Zick <brian@zick.io>
2019-12-18 21:01:51 -0500

    fix login under dark theme

--- views/login.php
+++ views/login.php
@@ -4,6 +4,7 @@
 <meta name=viewport content="width=device-width, initial-scale=1">
 <title>Login</title>
 <style>
+:root              { color-scheme: light dark; }
 *                  { color:#615555; line-height:1.6; font-family:lucida grande,segoe ui,roboto,sans-serif }
 body               { background:#FEFFFA; margin:0; text-align:center }
 input              { color:#444; border:2px solid #989e9a; padding:1.3% 3%; font:inherit }
@@ -13,6 +14,10 @@ input:focus        { outline:0; border-color:#333 }
 [role=notice]      { background:#F48A5D; color:#744955; padding:2.5%; text-align:center; margin:0 } 
 [role=notice] a    { color:#C36346; border:0 }
 .wrapper           { margin: 7%; }
+@media (prefers-color-scheme: dark) {
+  *                { color: rgb(210, 207, 211); }
+  body             { background: rgb(22, 23, 22); }
+}
 </style>
 
 <?php partial("alert.php", get_defined_vars()); ?>
