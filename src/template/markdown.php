38b858c0eb32f788ec47bdc4637f8fc5ea8af6c7
Brian Zick <brian@zick.io>
2019-12-18 20:58:42 -0500

    correct visual glitch with todo items

--- src/template/markdown.php
+++ src/template/markdown.php
@@ -4,11 +4,12 @@ function markdown($_) {
     $_ = preg_replace(
         "/(<~([^>]+)>)/", '<a href="/$2">$2</a>', $_);
     $_ = preg_replace(
-        "/[\*-] +\[ ?\]/", '- <input type=checkbox>', $_);
+        "/[\*-] +\[ ?\]/", '- <input todo type=checkbox>', $_);
     $_ = preg_replace(
-        "/[\*-] +\[x\]/", '- <input type=checkbox checked>', $_);
+        "/[\*-] +\[x\]/", '- <input todo type=checkbox checked>', $_);
     $_ = redlinks($_);
-
-    return (new \Michelf\MarkdownExtra)->transform($_);
+    $_ = (new \Michelf\MarkdownExtra)->transform($_);
+    $_ = preg_replace("/<li><input todo/", "<li todo><input", $_);
+    return $_;
 }
 
