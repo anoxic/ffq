d0d5260f717b7c55cf489880955b5019db5f407b
Brian Zick <brian@zick.io>
2022-05-28 11:42:27 +0000

    even simpler routes

--- src/routes.php
+++ src/routes.php
@@ -3,29 +3,24 @@ const WK = '.well-known/';
 
 function route(string $method, string $uri)
 {
-    [$code, $body] = match (true) {
-        prefix($uri, ':') => handle('edit', $uri),
-        prefix($uri, '!') => handle('delete', $uri),
-        prefix($uri, '=') => handle('login', $uri),
-        prefix($uri, '-') => handle('logout', $uri),
-        prefix($uri,  WK) => handle('letsencrypt', $uri),
-        default           => handle('view', $uri),
+    $handler = match (true) {
+        prefix($uri, ':') => 'edit',
+        prefix($uri, '!') => 'delete',
+        prefix($uri, '=') => 'login',
+        prefix($uri, '-') => 'logout',
+        prefix($uri,  WK) => 'letsencrypt',
+        default           => 'view',
     };
 
-    return [$code, $body];
-}
-
-function prefix(string $uri, string $sigil)
-{
-    return substr($uri, 1, strlen($sigil)) == $sigil;
-}
-
-function handle(string $view)
-{
-    if (file_exists($v = "handlers/$view.php")) {
+    if (file_exists($v = "handlers/$handler.php")) {
         ob_start();
         require $v;
         return [$code ?? 200, ob_get_clean()];
     }
     return [404, "<h1 title=\"$v\">404</h1>"];
+}
+
+function prefix(string $uri, string $sigil)
+{
+    return substr($uri, 1, strlen($sigil)) == $sigil;
 }
