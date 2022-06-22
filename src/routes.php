2194e17efbde19a996c7ee717bc2782f4250abec
Brian Zick <brian@zick.io>
2022-06-22 01:06:21 +0000

    redirect by setting a $redirect var in the handler

--- src/routes.php
+++ src/routes.php
@@ -15,7 +15,12 @@ function route(string $method, string $uri, ?array $post)
     if (file_exists($v = "handlers/$handler.php")) {
         ob_start();
         require $v;
-        return [$code ?? 200, ob_get_clean()];
+        $body = ob_get_clean();
+        if (!empty($redirect)) {
+            return [302, $redirect];
+        } else {
+            return [$code ?? 200, $body];
+        }
     }
     return [404, "<h1 title=\"$v\">404</h1>"];
 }
