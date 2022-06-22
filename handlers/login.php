58ce88e2d28f2f828af50402b88cdc5cdaea52ed
Brian Zick <brian@zick.io>
2022-06-22 20:07:43 +0000

    consistent quote pattern

--- handlers/login.php
+++ handlers/login.php
@@ -21,8 +21,8 @@ if ($u) {
 <meta name=viewport content="width=device-width,initial-scale=1,maximum-scale=1,shrink-to-fit=n,viewport-fit=cover">
 <meta name=apple-mobile-web-app-capable content=yes>
 <meta name=mobile-web-app-capable content=yes>
-<meta name="apple-mobile-web-app-status-bar-style" content="default">
-<meta name="theme-color" content="#872e4e">
+<meta name=apple-mobile-web-app-status-bar-style content=default>
+<meta name=theme-color content=#872e4e>
 
 <?=$error?>
 
