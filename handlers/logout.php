000ff2faef835655a325fee4e9443c42f7feca60
Brian Zick <brian@zick.io>
2022-06-23 15:21:27 +0000

    send cookie headers in a way that works

--- handlers/logout.php
+++ handlers/logout.php
@@ -1,6 +1,6 @@
 <?php
-$headers['Set-Cookie'] = 'wiki_session=.; expires=Thu, 01 Jan 1970 00:00:00 GMT; '
-                       . 'wiki_user=.; expires=Thu, 01 Jan 1970 00:00:00 GMT';
+$headers["Set-Cookie"]  = "wiki_user=; Max-Age=0;";
+$headers["Set-Cookie "] = "wiki_session=; Max-Age=0;"; // trailing space tricks swoole into sending multiple headers
 $redirect = '/';
 echo 'You have been logged out.';
 
