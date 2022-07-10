3bd6b5f6e9e57f039ecce2e03a5a81ed68f69f3e
Brian Zick <brian@zick.io>
2022-07-10 21:35:59 +0000

    use actually correct html structure

--- handlers/login.php
+++ handlers/login.php
@@ -24,38 +24,44 @@ if ($u) {
 }
 ?>
 <!doctype html>
-<meta charset=utf-8>
-<meta name=viewport content="width=device-width,initial-scale=1,maximum-scale=1,shrink-to-fit=n,viewport-fit=cover">
-<meta name=apple-mobile-web-app-capable content=yes>
-<meta name=mobile-web-app-capable content=yes>
-<meta name=apple-mobile-web-app-status-bar-style content=default>
-<meta name=theme-color content=#872e4e>
+<html lang=en>
+<head>
+    <meta charset=utf-8>
+    <meta name=viewport content="width=device-width,initial-scale=1,maximum-scale=1,shrink-to-fit=n,viewport-fit=cover">
+    <meta name=apple-mobile-web-app-capable content=yes>
+    <meta name=mobile-web-app-capable content=yes>
+    <meta name=apple-mobile-web-app-status-bar-style content=default>
+    <meta name=theme-color content=#872e4e>
+    <title>Login</title>
+<head>
+<body>
+    <?=$error?>
 
-<?=$error?>
+    <form method=post>
+        <div class=login>
+            <input name=u <?=$uf?> value="<?=$u?>" autocomplete=username placeholder=Username tabindex=1>
+            <input name=p <?=$pf?> type=password autocomplete=current-password placeholder=Password tabindex=2>
+        </div>
+        <input type=submit>
+    </form>
 
-<form method=post>
-    <div class=login>
-        <input name=u <?=$uf?> value="<?=$u?>" autocomplete=username placeholder=Username tabindex=1>
-        <input name=p <?=$pf?> type=password autocomplete=current-password placeholder=Password tabindex=2>
-    </div>
-    <input type=submit>
-</form>
+    <style>
+    *             { text-align: center; margin: 0; padding: 0; outline: none; font: inherit; background: inherit }
+    body          { background: #872e4e; font: bold 120%/1.5 times, serif }
+    input         { background: #141414; color: #f2efea; border: 0; border-radius: 5px; padding: .425em 0; caret-color: #872e4e }
+    ::placeholder { color: #872e4e }
+    [type=submit] { visibility: hidden }
+    .login        { width: 100%; display: flex; align-content: stretch; gap: .3em }
+    .login >*     { flex-grow: 1 }
 
-<style>
-*             { text-align: center; margin: 0; padding: 0; outline: none; font: inherit; background: inherit }
-body          { background: #872e4e; font: bold 120%/1.5 times, serif }
-input         { background: #141414; color: #f2efea; border: 0; border-radius: 5px; padding: .425em 0; caret-color: #872e4e }
-::placeholder { color: #872e4e }
-[type=submit] { visibility: hidden }
-.login        { width: 100%; display: flex; align-content: stretch; gap: .3em }
-.login >*     { flex-grow: 1 }
-
-@supports(padding: max(0px)) {
-    body {
-        padding-top: max(.6em, env(safe-area-inset-top));
-        padding-right: max(.6em, env(safe-area-inset-right));
-        padding-bottom: max(.6em, env(safe-area-inset-bottom));
-        padding-left: max(.6em, env(safe-area-inset-left));
+    @supports(padding: max(0px)) {
+        body {
+            padding-top: max(.6em, env(safe-area-inset-top));
+            padding-right: max(.6em, env(safe-area-inset-right));
+            padding-bottom: max(.6em, env(safe-area-inset-bottom));
+            padding-left: max(.6em, env(safe-area-inset-left));
+        }
     }
-}
-</style>
+    </style>
+</body>
+</html>
