99314729a773c9998656d2e70fd56d9683809f1f
Brian Zick <brian@zick.io>
2022-07-10 21:43:48 +0000

    create a set of meta tags for include

---
+++ src/meta.php
@@ -0,0 +1,6 @@
+    <meta charset=utf-8>
+    <meta name=viewport content="width=device-width,initial-scale=1,maximum-scale=1,viewport-fit=cover">
+    <meta name=apple-mobile-web-app-capable content=yes>
+    <meta name=mobile-web-app-capable content=yes>
+    <meta name=apple-mobile-web-app-status-bar-style content=default>
+    <meta name=theme-color content=#872e4e>
