2a9c5c86c96ae0c80fae5accf851246ce25c6f86
Brian Zick <brian@zick.io>
2022-07-11 20:18:05 +0000

    make whitespace display better

--- src/meta.php
+++ src/meta.php
@@ -1,4 +1,4 @@
-    <meta charset=utf-8>
+<meta charset=utf-8>
     <meta name=viewport content="width=device-width,initial-scale=1,maximum-scale=1,viewport-fit=cover">
     <meta name=apple-mobile-web-app-capable content=yes>
     <meta name=mobile-web-app-capable content=yes>
