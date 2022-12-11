ca8a82f7c5e6016cfca6d03ed3e62e5d21be904a
Brian Zick <brian@zick.io>
2022-12-11 15:55:51 +0000

    really basic textarea display

--- handlers/edit.php
+++ handlers/edit.php
@@ -1,8 +1,62 @@
 <?php
 
 if ($session('wiki_user')) {
-    echo "logged in";
 } else {
     echo "access denied (not logged in)";
     $code = 401;
+    return;
 }
+
+[$found, $file, $path] = tryfind('/' . substr($uri, 2), $children);
+?>
+<!doctype html>
+<html lang=en>
+<head>
+    <?php require('src/meta.php')?>
+	<title><?=basename($file)?> | editor</title>
+    <style>
+	textarea {
+	  width: 100%;
+	  height: 100%;
+	  position: absolute;
+	  top: 3em;
+	  left: 0;
+	}
+	button {
+	  position: fixed;
+	  top: 0;
+      display: block;
+	  width: 50%;
+	  height: 3em;
+      line-height: 2.5em;
+	}
+	button.save {
+	  right: 0;
+	  background: blue;
+	}
+	button.cancel {
+	  left: 0;
+	  background: gray;
+	}
+	* {
+	  background-color: white;
+	  color: black;
+	  border: 0;
+	}
+	@media (prefers-color-scheme: dark) {
+	  * {
+		background-color: black;
+		color: white;
+	  }
+	}
+    </style>
+</head>
+<body>
+    <form method=post>
+		<button class=cancel>Cancel</button>
+		<button class=save>Save</button>
+       <textarea name=file><?=file_get_contents($file)?></textarea>
+    </form>
+    <?php ?>
+</body>
+</html>
